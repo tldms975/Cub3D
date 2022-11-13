cc9024e3774fb4745078e18cea5890f76a6231d8
Hyeokjin Doo <hdoo@c8r1s10.42seoul.kr>
2022-11-13 11:55:13 +0900

    [UPDATE] t_world.h

--- srcs/includes/types/t_world.h
+++ srcs/includes/types/t_world.h
@@ -6,41 +6,50 @@
 /*   By: sielee <sielee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 16:38:59 by sielee            #+#    #+#             */
-/*   Updated: 2022/10/31 17:07:53 by sielee           ###   ########seoul.kr  */
+/*   Updated: 2022/11/07 21:24:22 by sielee           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef T_WORLD_H
 # define T_WORLD_H
-
+#define WIN_W 640//1200
+#define WIN_H 480//700
+#define DARKER 8355711
 typedef	enum
 {
 	NO,
 	SO,
 	WE,
-	EA
+	EA,
+	F,
+	C
 } t_dir;
 
-
+typedef struct s_rgb
+{
+	int	r;
+	int	g;
+	int	b;
+}				t_rgb;
 
 typedef struct	s_ceiling
 {
-	int	color;
+	int	rgb;
 }				t_ceiling;
 
 typedef struct	s_wall
 {
-	int	color;
+	int	rgb;
 }				t_wall;
 
 typedef struct	s_floor
 {
-	int	color;
+	int	rgb;
 }				t_floor;
 
 typedef struct	s_sprite
 {
-	int	color;
+	t_rgb	rgb;
 }				t_sprite;
 
 typedef struct	s_vec
@@ -49,21 +58,64 @@ typedef struct	s_vec
 	double	y;
 }				t_vec;
 
+typedef struct	s_ivec
+{
+	int	x;
+	int	y;
+}				t_ivec;
+
 typedef struct s_player
 {
-	double	speed;
 	t_vec	pos;
-	double	sight;
+	t_vec	dir;
+	t_vec	plane;
+	double	move_speed;
+	double	rot_speed;
 }				t_player;
+
+typedef struct s_texture
+{
+	double	wall_x;
+	int		type;
+	double	step;
+	double	pos;
+}				t_texture;
+
+typedef struct s_draw
+{
+	int		line_h;
+	int		start;
+	int		end;
+	t_ivec	tex;
+}				t_draw;
+
+
+typedef struct	s_raycast
+{
+	t_vec		ray;
+	t_ivec		block;
+	t_ivec		step;
+	t_vec		side;
+	t_vec		delta;
+	int			is_side;
+	double		cam_x;
+	double		d;
+}				t_raycast;
 
 typedef struct	s_world
 {
-	char		**map;
+	t_mlx		*tmlx;
+	int			**map;
+	int			buf[WIN_H][WIN_W];
+	int			**texture;
+	///char		**tex_path;
 	t_ceiling	ceiling;
 	t_wall		wall;
 	t_floor		floor;
-	int			txr[4];
-	t_player	*player;
+	t_sprite	*sprites;
+	t_player	player;
+	t_raycast	rc;
+	int			re;
 }				t_world;
 
 #endif
