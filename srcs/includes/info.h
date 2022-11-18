7203ed34585e9b96bf7dd465f55a9256a74a2fe6
Hyeokjin Doo <hdoo@c4r1s3.42seoul.kr>
2022-11-18 18:49:20 +0900

    info.h

--- srcs/includes/info.h
+++ srcs/includes/info.h
@@ -6,7 +6,7 @@
 /*   By: hdoo <hdoo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 00:33:21 by hdoo              #+#    #+#             */
-/*   Updated: 2022/11/17 17:16:57 by hdoo             ###   ########.fr       */
+/*   Updated: 2022/11/17 17:25:07 by hdoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 t_result	init_config(t_info *info, t_str_buf *line);
 bool		read_config(t_info *info);
 t_result	read_info(t_info *info);
-bool		parse_dot_cub(t_info *info, char* arg);
+bool		parse_dot_cub(t_info *info, char *arg);
 void		free_config(t_info *info);
 
 #endif // !INFO_H
