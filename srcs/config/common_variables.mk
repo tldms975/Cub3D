21e41800e5a10640ddaa27661780d2c4fa4ebe65
Hyeokjin Doo <hdoo@c2r5s2.42seoul.kr>
2022-11-19 04:42:47 +0900

    [Fix] makefile

--- srcs/config/common_variables.mk
+++ srcs/config/common_variables.mk
@@ -1,13 +1,12 @@
 Q			:= $(if $(filter 1,$(V) $(VERBOSE)),,@)
 MAKE		:= $(MAKE) $(if $(filter 1,$(V) $(VERBOSE)),,--no-print-directory) $(if $(filter 1,$(NO_ADDITIONAL_J)),,-j $(shell sh ../assets/nproc.sh))
 
-CC			:= clang
-CCLD		:= clang
+# CC			:= clang
+CCLD		:= cc
 AR			:= ar
 ARFLAGS		:= cr$(if $(filter 1,$(V) $(VERBOSE)),v,)
-CPPFLAGS	:= -I includes -I includes/types -I libft
-#$(shell find . -type d -mindepth 1 | grep -v .git | grep -v .cache | sed "s/^/-I /" | xargs)
-CFLAGS		:=  $(CPPFLAGS) #-Wall -Wextra -Werror #-sstd=c99 -pedantic 
+CPPFLAGS	:= $(shell find . -type d -mindepth 1 | grep -v .git | sed "s/^/-I /" | xargs)
+CFLAGS		:= -Wall -Wextra -Werror -std=c99 -pedantic $(CPPFLAGS)
 
 SRCS		:= $(shell find . -name "*.c" | grep -v .cache | grep -v test | grep -v bonus | xargs)
 SRCS_LIBFT	:= $(shell find . -name "ft_*.c" | grep -v .cache  | grep -v test | xargs)
