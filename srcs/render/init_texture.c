55b44b53851527dc9c343f7b9184ff8c7ea3a7ed
Hyeokjin Doo <hdoo@c1r1s3.42seoul.kr>
2022-11-22 05:48:00 +0900

    [Fix] fin

--- srcs/render/init_texture.c
+++ srcs/render/init_texture.c
@@ -6,7 +6,7 @@
 /*   By: sielee <sielee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 01:01:59 by sielee            #+#    #+#             */
-/*   Updated: 2022/11/21 20:43:31 by hdoo             ###   ########.fr       */
+/*   Updated: 2022/11/22 05:38:45 by hdoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ int	*ft_load_image(char *path, t_mlx *tmlx, int i)
 
 	tmlx->timg_texture[i].img = mlx_xpm_file_to_image(tmlx->mlx, path, &tmlx->timg_texture[i].w, \
 	&tmlx->timg_texture[i].h);
-
 	tmlx->timg_texture[i].data = (int *)mlx_get_data_addr(tmlx->timg_texture[i].img, \
 	&tmlx->timg_texture[i].bpp, &tmlx->timg_texture[i].line_len, &tmlx->timg_texture[i].endian);
 	res = (int *)malloc(sizeof(int) * (tmlx->timg_texture[i].w * tmlx->timg_texture[i].h));
@@ -54,41 +53,3 @@ void	ft_load_texture(t_info *info)
 		i++;
 	}
 }
-
-// void	ft_load_texture(t_world *world)
-// {
-// 	t_image	timg;
-
-// 	ft_load_image(world->tmlx, world->texture[0], world->tex_path[0], &timg);
-// 	ft_load_image(world->tmlx, world->texture[1], world->tex_path[1], &timg);
-// 	ft_load_image(world->tmlx, world->texture[2], world->tex_path[2], &timg);
-// 	ft_load_image(world->tmlx, world->texture[3], world->tex_path[3], &timg);
-// }
-
-// int	**ft_init_texture(void)
-// {
-// 	int	**ret;
-
-// 	if (!(ret = (int **)malloc(sizeof(int *) * 8)))
-// 	{
-// 		return (0);
-// 	}
-// 	for (int i = 0; i < 8; i++)
-// 	{
-// 		if (!(ret[i] = (int *)malloc(sizeof(int) * (TEX_H * TEX_W))))
-// 		{
-// 			//free(world->texture[i], i);
-// 			//free(world->texture);
-// 			return (0);
-// 		}
-// 		//world->texture[i] = ft_load_texture(world->tex_path[i], tmlx);
-// 	}
-// 	for (int i = 0; i < 8; i++)
-// 	{
-// 		for (int j = 0; j < TEX_H * TEX_W; j++)
-// 		{
-// 			ret[i][j] = 0;
-// 		}
-// 	}
-// 	return (ret);
-// }
--- srcs/render/set_world.c
+++ srcs/render/set_world.c
@@ -44,12 +44,12 @@ void	ft_wear_texture(t_world *world, t_raycast *rc, t_texture *tex, t_draw *dr)
 			tex->type = EA;
 	}
 	tex->wall_x -= floor(tex->wall_x);
-	dr->tex.x = (int)(tex->wall_x * world->tmlx->timg_texture[tex->type].w);
+	dr->tex.x = (int)(tex->wall_x * (double)world->tmlx->timg_texture[tex->type].w);
 	if ((rc->is_side == 0 && rc->ray.x > 0) \
 	|| (rc->is_side == 1 && rc->ray.y < 0))
 		dr->tex.x = world->tmlx->timg_texture[tex->type].w - dr->tex.x - 1;
 	tex->step = 1.0 * world->tmlx->timg_texture[tex->type].h  / dr->line_h;
-	tex->pos = (dr->start - world->screen_h / (double)(2 + dr->line_h) / 2) * tex->step;
+	tex->pos = (dr->start - world->screen_h / 2.0 + dr->line_h / 2.0) * tex->step;
 }
 
 void	ft_fill_buf(t_world *world, t_raycast *rc, t_texture *tex, size_t x)
