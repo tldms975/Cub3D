cda776d83c48fd2a9fe83b3fd927487be9621c7a
Hyeokjin Doo <hdoo@c1r1s3.42seoul.kr>
2022-12-06 19:12:08 +0900

    [FIX] color parsing

--- srcs/colors/parse_color.c
+++ srcs/colors/parse_color.c
@@ -6,7 +6,7 @@
 /*   By: hdoo <hdoo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 00:28:18 by hdoo              #+#    #+#             */
-/*   Updated: 2022/12/05 21:34:01 by hdoo             ###   ########.fr       */
+/*   Updated: 2022/12/06 19:09:53 by hdoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,68 @@
 #include "colors.h"
 #include <stdio.h>
 
+int	atoi_precheck(char *target)
+{
+	int	i;
+
+	i = 0;
+	if (target[i] == '-')
+	{
+		i++;
+	}
+	while (target != NULL
+		&& target[i] != '\0'
+		&& target[i] != '\n'
+		&& ft_isdigit(target[i]))
+	{
+		i++;
+	}
+	return (target[i] == '\0' || target[i] == '\n');
+}
+
+t_result	atoi_postcheck(int result, char *origin)
+{
+	if ((result == 0 && origin[0] != '0')
+		|| (result == -1 && !(origin[0] == '-' && origin[1] == '1')))
+	{
+		ft_putstr_fd("Error: Color input overflowed\n", 2);
+		return (ERROR);
+	}
+	else if (result < 0)
+	{
+		ft_putstr_fd("Error: Color out of range: Minus value\n", 2);
+		return (FAILURE);
+	}
+	return (SUCCESS);
+}
+
+t_result	strict_atoi(char **colors, int *raw_color, int i)
+{
+	if (atoi_precheck(colors[i]) == SUCCESS)
+	{
+		raw_color[i] = ft_atoi(colors[i]);
+		if (atoi_postcheck(raw_color[i], colors[i]) == SUCCESS)
+		{
+			return (SUCCESS);
+		}
+	}
+	return (FAILURE);
+}
+
 t_result	extract_color(char **colors, int *raw_color)
 {
 	int			i;
 	t_result	result;
 
-	result = SUCCESS;
+	result = FAILURE;
 	i = 0;
 	while (i < 3 && colors[i] != NULL)
 	{
-		raw_color[i] = ft_atoi(colors[i]);
+		result = strict_atoi(colors, raw_color, i);
+		if (result != SUCCESS)
+		{
+			break ;
+		}
 		i++;
 	}
 	if (i != 3)
